ce5ec60f96e3a39a6f6544bd138436b1ccb1773b
kanoonkayma <kanoonkaymas@hotmail.com>
2024-04-04 20:11:20 -0300

    Adicionado novas pastas ao repositório

---
+++ bookstore-mysql/src/main/java/com/example/bookstore/controller/BookController.java
@@ -0,0 +1,33 @@
+package com.example.bookstore.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.repository.BookRepository;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    // Adicionar métodos para atualização e exclusão conforme necessário
+}
---
+++ bookstore-mysql/src/main/java/com/example/bookstore/model/Book.java
@@ -0,0 +1,53 @@
+package com.example.bookstore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // Construtor padrão
+    public Book() {
+    }
+
+    // Construtor com parâmetros
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
